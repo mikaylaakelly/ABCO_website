cbed9d0de62581eaf979b66a4fd3c4e668e1dc6f
Mikayla <149916992+mikaylaakelly@users.noreply.github.com>
2025-10-27 17:31:33 -0400

    updated equipment list, fixed javascript, fixed example parser, fixed contact us to match javascript

--- abcoJavaScript.js
+++ abcoJavaScript.js
@@ -1,31 +1,40 @@
-
-
-function _(id){return document.getElementById(id);}
-
-function submitForm(){
-	_("mybtn").disabled = true;
-	_("status").innerHTML = 'please wait...';
-	var formdata = new FormData();
-	
-	window.alert("it made it past formdata object");
-	
-	formdata.append("n", _("n").value);
-	formdata.append("e", _("e").value);
-	formdata.append("m", _("m").value);
-	var ajax = new XMLHttpRequest();
-	
-	window.alert("it made it past ajax object");
-	
-	ajax.open("POST", "example_parser.php");
-	ajax.onreadystatechange = function(){
-		if(ajax.readystate == 4 && ajax.status == 200){
-			if(ajax.responseText == "Success"){
-				_("my_form").innerHTML = '<h2>Thanks '+ _("n").value + ' , your message has been sent</h2>';
-			}else{
-				_("status").innerHTML = ajax.responseText;
-				_("mybtn").disabled = false;
-			}
-		}
-	}
-	window.alert("It ran through all the javascript");
+function _(id) {
+  return document.getElementById(id);
+}
+
+function submitForm() {
+  // Disable submit button and show status
+  _("mybtn").disabled = true;
+  _("status").innerHTML = "Please wait...";
+
+  // Collect form data
+  var formdata = new FormData();
+  formdata.append("n", _("n").value); // name
+  formdata.append("e", _("e").value); // email
+  formdata.append("m", _("m").value); // message
+
+  // Create AJAX request
+  var ajax = new XMLHttpRequest();
+  ajax.open("POST", "example_parser.php", true);
+
+  // Handle response
+  ajax.onreadystatechange = function () {
+    if (ajax.readyState === 4 && ajax.status === 200) {
+      // Trim whitespace from response
+      var response = ajax.responseText.trim();
+
+      if (response === "Success") {
+        _("my_form").innerHTML =
+          "<h2>Thanks, " +
+          _("n").value +
+          "! Your message has been sent.</h2>";
+      } else {
+        _("status").innerHTML = response;
+        _("mybtn").disabled = false;
+      }
+    }
+  };
+
+  // Send data
+  ajax.send(formdata);
 }
--- example_parser.php
+++ example_parser.php
@@ -1,22 +1,33 @@
-<?php
-if(isset($_POST['n']) && isset($_POST['e']) && isset ($_POST['m']) ){
-	$n = $_POST['n'];
-	$e = $_POST['e'];
-	$m = n12($_POST['m']);
-	/* $to = "you@domain.com"; */
-	$to = $e; /*for privacy while being able to test */
-	$from = $e;
-	$subject = 'Contact form Message';
-	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
-	$headers = "From: $from\n";
-	$headers .= "MIME-Version: 1.0\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\n1";
-	if(mail($to, $subject, $message, $headers)){
-		echo"success!!";
-	}else{
-		echo"The server failed to send the message.  Please try again latter.";
-	}
-	
-}
-
-?>
+<?php
+if (isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m'])) {
+
+    $n = htmlspecialchars($_POST['n']);
+    $e = htmlspecialchars($_POST['e']);
+    $m = nl2br(htmlspecialchars($_POST['m']));
+
+    // Change this to your real business email once tested
+    // For now, using sender email is fine for local testing
+    $to = $e;  // <-- Replace with "you@yourdomain.com" when ready
+    $from = $e;
+    $subject = 'Contact Form Message from ABCO Website';
+
+    $message = "
+    <b>Name:</b> $n <br>
+    <b>Email:</b> $e <br><br>
+    <p>$m</p>
+    ";
+
+    $headers = "From: $from\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Success";
+    } else {
+        echo "The server failed to send the message. Please try again later.";
+    }
+
+} else {
+    echo "Form submission error: missing fields.";
+}
+?>

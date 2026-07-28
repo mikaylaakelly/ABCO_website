f98ba2e8f65397eceaf262c5b7989919053ffadd
Mikayla <149916992+mikaylaakelly@users.noreply.github.com>
2026-07-28 12:10:52 -0400

    hopefully fixing email

--- abcoJavaScript.js
+++ abcoJavaScript.js
@@ -3,44 +3,41 @@ function _(id) {
 }
 
 function submitForm() {
+
   _("mybtn").disabled = true;
   _("status").innerHTML = "Sending...";
 
-  var formdata = new FormData();
-  formdata.append("n", _("n").value);
-  formdata.append("e", _("e").value);
-  formdata.append("m", _("m").value);
-
-  var ajax = new XMLHttpRequest();
-
-  // IMPORTANT: this must match your GoDaddy file path
-  ajax.open("POST", "example_parser.php", true);
-
-  ajax.onload = function () {
-    _("mybtn").disabled = false;
-
-    if (ajax.status === 200) {
-      var response = ajax.responseText.trim();
-
-      if (response === "Success") {
-        _("status").innerHTML = "Message sent successfully!";
-        _("status").style.color = "lightgreen";
-        _("my_form").reset();
-      } else {
-        _("status").innerHTML = response;
-        _("status").style.color = "red";
-      }
-    } else {
-      _("status").innerHTML = "Server error. Try again later.";
-      _("status").style.color = "red";
-    }
+  var templateParams = {
+    name: _("n").value,
+    email: _("e").value,
+    message: _("m").value
   };
 
-  ajax.onerror = function () {
-    _("status").innerHTML = "Request failed.";
+  emailjs.send(
+    "service_sf64wdj",
+    "template_ji00hef",
+    templateParams
+  )
+  .then(function(response) {
+
+    console.log("SUCCESS", response);
+
+    _("status").innerHTML = "Message sent successfully!";
+    _("status").style.color = "lightgreen";
+
+    _("my_form").reset();
+    _("mybtn").disabled = false;
+
+  })
+  .catch(function(error) {
+
+    console.log("FAILED", error);
+
+    _("status").innerHTML = "Message failed to send. Please try again.";
     _("status").style.color = "red";
-    _("mybtn").disabled = false;
-  };
 
-  ajax.send(formdata);
+    _("mybtn").disabled = false;
+
+  });
+
 }
